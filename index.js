71366a3082241106e0531fb103ad4d5d2eeb1412
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2019-07-09 20:00:49 +0300

    readme updated

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 const QRCode = require('qrcode');
 const app = require('express')()
-
 app.get('/:data', (req, res) => {
-
     QRCode.toDataURL(req.params.data, { version: 4 }, function (err, url) {
         if (err) {
-            return res.status(404).send(err.message)
-
+            return res.status(400).send(err.message)
         }
         const im = url.split(",")[1];
-
         const img = Buffer.from(im, 'base64');
         res.writeHead(200, {
             'Content-Type': 'image/png',
             'Content-Length': img.length
         });
-
         res.end(img);
     })
 })
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log('server started')
+    console.log('server started at ' + port);
 })
