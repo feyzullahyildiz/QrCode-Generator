c33a0688ef0231cf10e5affa74ad048f02f883bd
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2019-07-09 08:11:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const QRCode = require('qrcode');
+const app = require('express')()
+
+app.get('/:data', (req, res) => {
+
+    QRCode.toDataURL(req.params.data, { version: 4 }, function (err, url) {
+        if (err) {
+            return res.status(404).send(err.message)
+
+        }
+        const im = url.split(",")[1];
+
+        const img = Buffer.from(im, 'base64');
+        res.writeHead(200, {
+            'Content-Type': 'image/png',
+            'Content-Length': img.length
+        });
+
+        res.end(img);
+    })
+})
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('server started')
+})
